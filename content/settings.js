c341ab9c62a6c78823ca2d609c7e040eea29e15d
Emily <captain.emily.blum@gmail.com>
2021-02-21 10:08:06 -0500

    Initial Commit

---
+++ content/settings.js
@@ -0,0 +1,28 @@
+const SETTINGS =
+{
+	STATSNUMTAGS: 25,
+	STATSNUMTYPE: 5,
+	LOADANIMNUM: 50,
+	WIDEARTICLE: true,
+	AUTOWIDETRIGGER: 7,
+	USEMASONRY: true,
+	MASONRYPROGRESS: true,
+	ARTICLEIDBASE: 'item',
+	SHOWUPPER: true,
+	SHOWTITLE: true,
+	SHOWDATE: false,
+	SHOWAUTH: true,
+	SHOWTYPE: false,
+	SHOWLINK: true,
+	SHOWLOWER: true,
+	SHOWTAGS: true,
+	SHOWPROJ: true,
+	SHOWNOTE: true,
+	SHOWQOTE: true,
+	SHOWTERM: true,
+	SHOWDONE: true,
+	SHOWPROG: true,
+	SHOWIMAG: true,
+	SHOWFILE: true,
+	SHOWOVERLAY: true
+}
